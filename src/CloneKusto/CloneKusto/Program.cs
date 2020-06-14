30b6cc5f12a00cfbe29a607d8cedb80dc0500701
Akil Burgess <akilburgess@gmail.com>
2020-06-14 15:34:25 -0400

    Add CloneKusto sln and project

---
+++ src/CloneKusto/CloneKusto/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace CloneKusto
+{
+    class Program
+    {
+        static Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            return Task.CompletedTask;
+        }
+    }
+}
